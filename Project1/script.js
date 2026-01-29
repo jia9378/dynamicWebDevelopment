4a8fc9ef312101b47904e32a7d363b18ff8a2370
Jiayang He <johnhe9378@gmail.com>
2026-01-29 02:52:01 -0500

    project 1 version 1 29012026

---
+++ Project1/script.js
@@ -0,0 +1,25 @@
+// let img
+// let x = 320;
+// let y = 180;
+// let xspeed = 5;
+// let yspeed = 2;
+// let r = image.width
+// function preload(){
+//     img = loadImage("blueDiamond.png")
+// }
+// function setup (){
+//     createCanvas(windowWidth, windowHeight);
+// }
+
+// function draw(){
+//     let r = img.width
+//     x += xspeed;
+//     y += yspeed;
+//     if (x > width - r || x < r) {
+//         xspeed = -xspeed;
+//     }
+//     if (y > height - r || y < r) {
+//         yspeed = -yspeed;
+//     }
+//     img(img,x,y)
+// }
