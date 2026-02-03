31942e20077c7feba313ed4ad4bb6340776055bf
Jiayang He <johnhe9378@gmail.com>
2026-02-03 18:39:59 -0500

    project 2 update 2

--- Project2/noise.js
+++ Project2/noise.js
@@ -7,7 +7,7 @@ function setup(){
 
   g = createGraphics(width, height);
   g.loadPixels();
-  frameRate(12);
+  frameRate(8);
 
   // for(let i = 0; i < g.pixels.length; i += 4){
   //   let n = random(10, 100);
@@ -18,19 +18,21 @@ function setup(){
   // }
 
   // g.updatePixels();
-  noLoop();
+  // noLoop();
 }
 
 function draw(){
-  background("#E5D6A5");
+  background("#EDD68C");
   for(let i = 0; i < g.pixels.length; i += 4){
-    let n = random(10, 100);
+    let n = random(0, 10);
     g.pixels[i]   = n;
     g.pixels[i+1] = n;
     g.pixels[i+2] = n;
-    g.pixels[i+3] = random(0, 100); // alpha
+    g.pixels[i+3] = random(0, 130); // alpha
   }
 
   g.updatePixels();
   image(g, 0, 0);
+  fill
+  circle (mouseX, mouseY, 30);
 }
