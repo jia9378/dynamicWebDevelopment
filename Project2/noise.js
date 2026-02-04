49e5a0b1d229f3deff46f5486c532d74b00f097b
Jiayang He <johnhe9378@gmail.com>
2026-02-04 01:23:23 -0500

    project 2 update 4

--- Project2/noise.js
+++ Project2/noise.js
@@ -1,13 +1,16 @@
 let g;
-
+let mouse
+let stamps = [];
+function preload() {
+  mouse = loadImage('img/Tube0.png');
+}
 function setup(){
   let c = createCanvas(windowWidth, windowHeight);
   c.parent("noise-layer");
-  pixelDensity(1);
-
+  pixelDensity(0.75);
   g = createGraphics(width, height);
   g.loadPixels();
-  frameRate(8);
+  frameRate(10);
 
   // for(let i = 0; i < g.pixels.length; i += 4){
   //   let n = random(10, 100);
@@ -24,15 +27,46 @@ function setup(){
 function draw(){
   background("#EDD68C");
   for(let i = 0; i < g.pixels.length; i += 4){
-    let n = random(0, 10);
-    g.pixels[i]   = n;
+    let n = random(0, 100);
+    g.pixels[i]   = n+50;
     g.pixels[i+1] = n;
-    g.pixels[i+2] = n;
-    g.pixels[i+3] = random(0, 130); // alpha
+    g.pixels[i+2] = n-90;
+    g.pixels[i+3] = random(0, 220); // alpha
   }
 
   g.updatePixels();
   image(g, 0, 0);
-  fill
-  circle (mouseX, mouseY, 30);
+  for (let i = 0; i < stamps.length; i++) {
+    let s = stamps[i];
+    image(mouse, s.x - s.w/2, s.y - s.h/2, s.w, s.h);
+  }
+  // circle (mouseX, mouseY, 30);
+  // let w = mouseIsPressed ? 160 : 80;
+  // let h = mouseIsPressed ? 130 : 65;
+  let w = 80;
+  let h = 65;
+  if (mouseIsPressed){
+    w = 160;
+    h = 130;
+  } else {
+    w = 80;
+    h = 65;
+  }
+
+
+  image(mouse, mouseX - w/2, mouseY - h/2, w, h);
+  
+  // image(mouse, mouseX-40, mouseY-30,80,65);
+  // mouseResize();
+}
+
+// function mouseResize(){
+//   if (mouseIsPressed){
+//     scale(2);
+//     image(mouse, mouseX-40, mouseY-30,80,65);
+//   }
+// }
+
+function mouseClicked() {
+  stamps.push({ x: mouseX, y: mouseY, w: 80, h: 65 });
 }
