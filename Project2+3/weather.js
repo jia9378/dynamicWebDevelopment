174fb77af6501938f2052c4f7f82771cedbc0482
Jiayang He <johnhe9378@gmail.com>
2026-02-12 00:25:28 -0500

    project 2+3 update 2

--- Project2+3/weather.js
+++ Project2+3/weather.js
@@ -1,6 +1,10 @@
 let g;
-let mouse
+let mouse0;
+let mouse1;
 let stamps = [];
+let rain = 1;
+
+
 console.log("weather.js loaded");
 window.onload = async() => {
     console.log("the page has loaded");
@@ -18,21 +22,40 @@ window.onload = async() => {
     console.log(data);
     let current = data.current;
     console.log(current);
-
+    let time = current.observation_time;
     let temperature = current.temperature;
     let weather_descriptions = current.weather_descriptions[0].toLowerCase();
     // weather_descriptions = "drizzle"
+    weather_descriptions = "sunny"
+    // weather_descriptions = "snowy"
+    document.getElementById("time").innerHTML = time;
     document.getElementById("today").innerHTML = "\r\n" + temperature + "°C, " + weather_descriptions;
-    document.getElementById("weatherImg").innerHTML = `<img src="img/rainy.gif" alt="">`;
+    // document.getElementById("weatherGif").src = `<img src="img/rainy.gif" alt="">`;
     
     if (weather_descriptions.includes("rain") || weather_descriptions.includes("drizzle") || weather_descriptions.includes("thunder")) {
-        document.getElementById("weatherImg").innerHTML = `<img src="img/rainy.gif" alt="">`;
+        document.getElementById("weatherGif").src = `img/rainy.gif`;
+        document.getElementById("telegraphyLine1").innerHTML = "Umbrellas bloom across the streets.";
+        rain = 1;
     } else if (weather_descriptions.includes("cloud") || weather_descriptions.includes("overcast")) {
-        document.getElementById("weatherImg").innerHTML = `<img src="img/cloudy.gif" alt="">`;
+        document.getElementById("weatherGif").src = `img/cloudy.gif`;
+        document.getElementById("telegraphyLine1").innerHTML = "The horizon hesitates.";
+        rain = 0;
+    } else if (weather_descriptions.includes("sun") || weather_descriptions.includes("clear")) {
+        document.getElementById("weatherGif").src = `img/sunny.gif`;
+        document.getElementById("telegraphyLine1").innerHTML = "Light spills without apology.";
+        rain = 0;
+    } else if (weather_descriptions.includes("snow") || weather_descriptions.includes("sleet") || weather_descriptions.includes("hail")) {
+        document.getElementById("weatherGif").src = `img/snowy.gif`;
+        document.getElementById("telegraphyLine1").innerHTML = "Snowflakes dance in the cold air.";
+        rain = 1;
+    } else {
+        document.getElementById("weatherGif").src = `img/placeHolder.png`;
+        rain = 0;
     }
 }
 function preload() {
-  mouse = loadImage('img/Tube0.png');
+  mouse1 = loadImage('img/umbrella1.png');
+  mouse0 = loadImage('img/umbrella0.png');
 }
 function setup(){
   let c = createCanvas(windowWidth, windowHeight);
@@ -59,23 +82,31 @@ function draw(){
 
   for (let i = 0; i < stamps.length; i++) {
     let s = stamps[i];
-    image(mouse, s.x - s.w/2, s.y - s.h/2, s.w, s.h);
+    if (rain == 1){ 
+      image(mouse1, s.x - s.w/2, s.y - s.h/2, s.w, s.h);
+    } else {
+      image(mouse0, s.x - s.w/2, s.y - s.h/2, s.w, s.h);
+    }
   }
   // Set doubled width and height based on mouse press
-  let w = 80;
-  let h = 65;
+  let w = 100;
+  let h = 100;
   if (mouseIsPressed){
-    w = 160;
-    h = 130;
+    w = 200;
+    h = 200;
   } else {
-    w = 80;
-    h = 65;
+    w = 100;
+    h = 100;
   }
   // Draw the mouse image at the mouse position
-  image(mouse, mouseX - w/2, mouseY - h/2, w, h);
-  
+  if (rain == 1) {
+    image(mouse1, mouseX - w/2, mouseY - h/2, w, h);
+  } else {
+    image(mouse0, mouseX - w/2, mouseY - h/2, w, h);
+  }
 }
+
 // Add a stamp where the mouse is clicked
 function mouseClicked() {
-  stamps.push({ x: mouseX, y: mouseY, w: 80, h: 65 });
+  stamps.push({ x: mouseX, y: mouseY, w: 100, h: 100 });
 }
